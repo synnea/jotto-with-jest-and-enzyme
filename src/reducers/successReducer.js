2378d7e01b6996653c5cedef59c0143f9927cb49
Carina Poell <carrie.poell@gmail.com>
2021-02-10 11:30:42 +0100

    write code and tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,6 +1,11 @@
 
+import {actionTypes} from '../actions';
 
-
-export default (state, action) => {
-    return null;
+export default (state=false, action) => {
+    switch (action.type) {
+        case (actionTypes.CORRECT_GUESS):
+            return true;
+        default:
+            return state;
+    }
 }
