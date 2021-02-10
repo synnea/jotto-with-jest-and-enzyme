66b4c2c179b6e63c2c25098bfa8567ace88cf1ca
Carina Poell <carrie.poell@gmail.com>
2021-02-10 17:44:27 +0100

    set up guessedWords tests

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { findByTestAttr, storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('updates the state correcly for unsuccessful guess', () => {
+
+        });
+        test('updates the state correcly for successful guess', () => {
+
+        });
+    });
+    describe('some guessed words', () => {
+        test('updates the state correcly for unsuccessful guess', () => {
+
+        });
+        test('updates the state correcly for successful guess', () => {
+
+        });
+    })
+});
